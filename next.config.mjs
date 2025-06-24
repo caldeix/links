ff768340614a425f85cdc7a87b071574efbb642a
Caldeiro, Luis M. <caldeiro@protonmail.com>
2025-06-24 21:34:07 +0200

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/links' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/links/' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/links' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/links/' : '',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+export default nextConfig
